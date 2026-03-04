e9c3c14e5b3a6a982f4cb80efd947de1db138159
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-03-05 01:18:37 +0600

    #

--- Milestone 5/Module 28/28.3 fetch.js
+++ Milestone 5/Module 28/28.3 fetch.js
@@ -1,3 +1,17 @@
 // JSON structural model
     //
 
+// let api = fetch('https://jsonplaceholder.typicode.com/posts/1');
+// console.log(api); // Promise { <pending> }
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+//1. fetch() gives a promise
+//2. we can use .then() to get the response of the promise
+//3. we can use .json() to convert the response to json format
+//4. we can use another .then() to get the json data and log it to the console
