ea0414ca86b88a91ba7509033801c1d200541d55
null <null>
2026-03-05 08:19:20 +0600

    #

--- Milestone 5/Module 28/28.4 - 28.8 DummyJSON/script.js
+++ Milestone 5/Module 28/28.4 - 28.8 DummyJSON/script.js
@@ -1,7 +1,7 @@
 let oneClick = document.getElementById("oneClick");
 
 oneClick.addEventListener("click", () => {
-  fetch("https://dummyjson.com/products") // promise of response
+  fetch("https://dummyjson.com/products/1") // promise of response
     .then((res) => res.json()) // res.json() is roughly equivalent to: res.text().then(text => JSON.parse(text)) -- Which means whatever it'll pass to data is an object.
     .then((data) => {
         let x = document.createElement('div');
@@ -12,13 +12,42 @@ oneClick.addEventListener("click", () => {
 });
 
 let loadPost = document.getElementById("loadPost");
+let arr = []
+for (let i = 0; i <= 100; i++){
+  arr[i] = `https://dummyjson.com/products/${i}`;
+}
 
+console.log(arr);
+
+//2
 loadPost.addEventListener("click", () => {
   let url = "https://dummyjson.com/products";
   fetch(url).then(response => response.json()).then(jsonData => {
-    let x = document.createElement("div");
-    x.innerHTML = `<br><code>${JSON.stringify(jsonData)}</code><br>`;
-    document.querySelector("body").appendChild(x);
-    console.log(jsonData);
+    jsonData.forEach((Data) => { 
+        let x = document.createElement("div");
+        x.innerHTML = `<br><code>${Data}</code><br>`;
+        document.querySelector("body").appendChild(x);
+    })
   })
 });
+
+// 2
+// loadPost.addEventListener("click", () => {
+//   let url = "https://dummyjson.com/products";
+//   fetch(url).then(response => response.json()).then(jsonData => {
+//     products(jsonData)
+//     jsonData.forEach((Data) => { 
+//         let x = document.createElement("div");
+//         x.innerHTML = `<br><code>${Data}</code><br>`;
+//         document.querySelector("body").appendChild(x);
+//     })
+//   })
+// });
+
+// let products = (json) => {
+//   json.forEach((Data) => { 
+//     let x = document.createElement("div");
+//     x.innerHTML = `<br><code>${Data}</code><br>`;
+//     document.querySelector("body").appendChild(x);
+//   })
+// }
