b5f953ca081d960d378aa3b039fd642128a4ac84
null <null>
2026-03-05 07:24:48 +0600

    #

--- Milestone 5/Module 28/28.4 - 28.8 DummyJSON/script.js
+++ Milestone 5/Module 28/28.4 - 28.8 DummyJSON/script.js
@@ -1,7 +1,7 @@
 let oneClick = document.getElementById("oneClick");
 
 oneClick.addEventListener("click", () => {
-  fetch("https://jsonplaceholder.typicode.com/posts/1") // promise of response
+  fetch("https://dummyjson.com/products") // promise of response
     .then((res) => res.json()) // res.json() is roughly equivalent to: res.text().then(text => JSON.parse(text)) -- Which means whatever it'll pass to data is an object.
     .then((data) => {
         let x = document.createElement('div');
@@ -14,7 +14,7 @@ oneClick.addEventListener("click", () => {
 let loadPost = document.getElementById("loadPost");
 
 loadPost.addEventListener("click", () => {
-  let url = "https://jsonplaceholder.typicode.com/posts";
+  let url = "https://dummyjson.com/products";
   fetch(url).then(response => response.json()).then(jsonData => {
     let x = document.createElement("div");
     x.innerHTML = `<br><code>${JSON.stringify(jsonData)}</code><br>`;
